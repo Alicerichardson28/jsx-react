ba3b8fc2c234d77fc52f54ccc8b0d3f357022fa2
Parada <paradafaii28@gmail.com>
2021-05-28 10:09:05 -0400

    set up react from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// if (module.hot) {
+//     module.hot.accept();
+//   }
+
+// create a react component
+const App = () => {
+    return (
+        <div>
+            <label 
+                className="label" 
+                for="name" >
+                    Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button 
+                style={{ backgroundColor: 'blue', color: 'white' }}>
+                Submit  
+            </button>
+        </div>
+    )
+}
+
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+     <App/>,
+     document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
